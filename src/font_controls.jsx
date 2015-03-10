11e9c84bcdd4820079687dfb20a1fc3d3de12d82
Jacob Gardner <jacob.gardner@garmin.com>
2015-03-10 17:47:00 -0500

    Send generate command

--- src/font_controls.jsx
+++ src/font_controls.jsx
@@ -1,7 +1,74 @@
+/*jslint browser: true */
+/*global FileReader */
 'use strict';
 var React = require('react');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+        return {
+            fileName: '',
+            fileData: null,
+            fontSize: 64,
+            spread: 20,
+            inSize: 1024,
+            outSize: 512,
+            distField: true,
+        };
+    },
+    updateFontSize: function (evt) {
+        this.setState({
+            fontSize: Number(evt.target.value),
+        });
+    },
+    updateSpread: function (evt) {
+        this.setState({
+            spread: Number(evt.target.value),
+        });
+    },
+    updateInSize: function (evt) {
+        this.setState({
+            inSize: Number(evt.target.value),
+        });
+    },
+    updateOutSize: function (evt) {
+        this.setState({
+            outSize: Number(evt.target.value),
+        });
+    },
+    updateDistField: function (evt) {
+        this.setState({
+            distField: evt.target.checked,
+        });
+    },
+    updateFile: function () {
+        var files = this.refs.file_input.getDOMNode().files,
+            reader = new FileReader(),
+            self = this;
+
+        reader.onload = function () {
+            self.setState({
+                fileName: files[0].name,
+                fileData: this.result,
+            });
+        };
+
+        reader.readAsArrayBuffer(files[0]);
+
+    },
+    generate: function (evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+
+        this.props.generateFont(
+            this.state.fileData,
+            this.state.fontSize,
+            this.state.spread,
+            this.state.inSize,
+            this.state.outSize,
+            this.state.distField
+        );
+
+    },
     render: function () {
         return (
             <div>
@@ -12,10 +79,10 @@ module.exports = React.createClass({
                             <div className="input-group">
                                 <span className="input-group-btn">
                                     <span className="btn btn-default btn-file">
-                                        Browse&hellip; <input type="file" />
+                                        Browse&hellip; <input ref="file_input" type="file" onChange={this.updateFile} />
                                     </span>
                                 </span>
-                                <input type="text" className="form-control" readOnly />
+                                <input type="text" value={this.state.fileName} className="form-control" readOnly />
                             </div>
                         </div>
                     </div>
@@ -23,28 +90,28 @@ module.exports = React.createClass({
                     <div className="form-group">
                         <label className="col-sm-4 control-label">Font Size</label>
                         <div className="col-sm-6">
-                            <input type="text" defaultValue={64} className="form-control" placeholder="Font Size" />
+                            <input type="text" value={this.state.fontSize} onChange={this.updateFontSize} className="form-control" placeholder="Font Size" />
                         </div>
                     </div>
 
                     <div className="form-group">
                         <label className="col-sm-4 control-label">Spread</label>
                         <div className="col-sm-6">
-                            <input type="text" defaultValue={20} className="form-control" placeholder="Spread" />
+                            <input type="text" value={this.state.spread} onChange={this.updateSpread} className="form-control" placeholder="Spread" />
                         </div>
                     </div>
 
                     <div className="form-group">
                         <label className="col-sm-4 control-label">Font BMP Size</label>
                         <div className="col-sm-6">
-                            <input type="text" defaultValue={1024} className="form-control" placeholder="Font BMP Size" />
+                            <input type="text" value={this.state.inSize} onChange={this.updateInSize} className="form-control" placeholder="Font BMP Size" />
                         </div>
                     </div>
 
                     <div className="form-group">
                         <label className="col-sm-4 control-label">Distance Field BMP Size</label>
                         <div className="col-sm-6">
-                            <input type="text" defaultValue={512} className="form-control" placeholder="Distance Field BMP Size" />
+                            <input type="text" value={this.state.outSize} onChange={this.updateOutSize} className="form-control" placeholder="Distance Field BMP Size" />
                         </div>
                     </div>
 
@@ -52,7 +119,7 @@ module.exports = React.createClass({
                         <div className="col-sm-offset-4 col-sm-10">
                             <div className="checkbox">
                                 <label>
-                                    <input type="checkbox" /> Build Distance Field <br />
+                                    <input type="checkbox" checked={this.state.distField} onChange={this.updateDistField} /> Build Distance Field <br />
                                 </label>
                             </div>
                         </div>
@@ -60,7 +127,7 @@ module.exports = React.createClass({
 
                     <div className="form-group">
                         <div className="col-sm-offset-4 col-sm-10">
-                            <button className="btn btn-default">Generate Font!</button>
+                            <button className="btn btn-default" onClick={this.generate}>Generate Font!</button>
                         </div>
                     </div>
                 </form>
